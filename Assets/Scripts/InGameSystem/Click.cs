7ca8d41e6fec2f466cd0423e846c817dca38d4df
DESKTOP-RSD374P\rlqma <rlqmalej@gmail.com>
2023-02-04 14:42:52 +0900

    touch hit

---
+++ Assets/Scripts/InGameSystem/Click.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public bool isGame = true;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && isGame)
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
+
+            if(hit.collider != null)
+            {
+                Debug.Log(hit.collider);
+                if(hit.collider.gameObject.tag == "Npc")
+                {
+                    Debug.Log("npc 를 클릭했습니다");
+                }
+            }
+        }
+           
+    }
+}
